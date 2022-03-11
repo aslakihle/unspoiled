ea077a0ec578838b9ccdf359f6ab02ff26e03a20
Aslak Ihle <aslak.ihle@gmail.com>
2022-03-11 14:43:20 +0100

    vercel update

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,72 +7,62 @@ export default {
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-  
-  
+  target: "static",
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'unspoiled',
+    title: "unspoiled",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
-// server: {
-//   https: {
-//     key: fs.readFileSync(path.resolve(__dirname, '../cert/ca-key.pem')),
-//     cert: fs.readFileSync(path.resolve(__dirname, '../cert/ca-cert.pem'))
-//   }
-// },
-  
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync(path.resolve(__dirname, '../cert/ca-key.pem')),
+  //     cert: fs.readFileSync(path.resolve(__dirname, '../cert/ca-cert.pem'))
+  //   }
+  // },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/css/global.css'
-  ],
+  css: ["@/assets/css/global.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '@/plugins/forward-set-cookies.js', mode: 'server' },
-  ],
+  plugins: [{ src: "@/plugins/forward-set-cookies.js", mode: "server" }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxtjs/fontawesome',
-  ],
+  buildModules: ["@nuxtjs/fontawesome"],
 
   fontawesome: {
     icons: {
       solid: true,
-      brands: true
-    }
+      brands: true,
+    },
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    'cookie-universal-nuxt',
+    "@nuxtjs/axios",
+    "cookie-universal-nuxt",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "http://localhost:5000/",
+    baseURL: process.env.baseURL,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
